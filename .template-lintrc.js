46a294a90a7474f0fb7ac2105ab6aee5749dc594
Adam Baker <bakerac4@gmail.com>
2019-10-24 20:44:19 -0400

    Updates

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,13 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  extends: 'recommended'
+    extends: ["recommended"],
+    rules: {
+        "no-bare-strings": true,
+        "block-indentation": 4,
+        "attribute-indentation": {
+            indentation: 4,
+            "open-invocation-max-len": 120
+        }
+    }
 };
